22e17a2216a8ff0eba8d52df167d787d5c5925c4
Jessica <ladyjtm@gmail.com>
2015-01-30 11:19:17 -0800

    Add functionality to band page.

--- app.rb
+++ app.rb
@@ -7,3 +7,19 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get('/bands') do
+  @bands = Band.all()
+  @venues = Venue.all()
+  erb(:band)
+end
+
+post("/bands") do
+  band_name = params.fetch("band_name")
+  # venue_name = params.fetch("venue_name")
+  band = Band.create({ :name => band_name})
+  # venue = Venue.create({ :name => venue_name})
+  # venue = Venue.find(params.fetch("venue_id").to_i())
+  # band.venues().push(venue)
+  redirect("/bands")
+end
